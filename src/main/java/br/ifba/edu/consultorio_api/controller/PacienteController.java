0a8afe03e8d23bdf0a4a37759e7ce01d85cc7132
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-06-24 22:53:52 -0300

    feat: cria paginação e ordenação de listagem de clientes

--- src/main/java/br/ifba/edu/consultorio_api/controller/PacienteController.java
+++ src/main/java/br/ifba/edu/consultorio_api/controller/PacienteController.java
@@ -2,9 +2,13 @@ package br.ifba.edu.consultorio_api.controller;
 
 import br.ifba.edu.consultorio_api.dto.PacienteResponseDTO;
 import br.ifba.edu.consultorio_api.dto.request.PacienteDTO;
+import br.ifba.edu.consultorio_api.entities.Paciente;
 import br.ifba.edu.consultorio_api.service.PacienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -18,14 +22,13 @@ public class PacienteController {
     @Autowired
     PacienteService pacienteService;
 
-    @GetMapping
-    public ResponseEntity<List<PacienteResponseDTO>> listaDePacientes(Pageable pageable) {
-        return pacienteService.listarTodosPacientes();
+    @GetMapping("/teste")
+    public ResponseEntity<Page<PacienteResponseDTO>> listarPacientes(
+            @PageableDefault(sort = "nome", direction = Sort.Direction.ASC, size = 10) Pageable pageable) {
+        Page<PacienteResponseDTO> pacientesDTO = pacienteService.listarPacientesOrdenadosEPageados(pageable);
+        return ResponseEntity.ok(pacientesDTO);
     }
-    @GetMapping("/buscar-por-nome")
-    public ResponseEntity<List<PacienteResponseDTO>> buscarPorNome(@RequestParam("nome") String nome) {
-        return pacienteService.listarPorNomePaciente(nome);
-    }
+
 
     @PostMapping
     public ResponseEntity<PacienteDTO> inserirPaciente(@RequestBody PacienteDTO pacienteDTO, UriComponentsBuilder builder) {
--- src/main/java/br/ifba/edu/consultorio_api/service/PacienteService.java
+++ src/main/java/br/ifba/edu/consultorio_api/service/PacienteService.java
@@ -5,9 +5,11 @@ import br.ifba.edu.consultorio_api.dto.request.PacienteDTO;
 import br.ifba.edu.consultorio_api.entities.Paciente;
 import br.ifba.edu.consultorio_api.repository.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -22,23 +24,11 @@ public class PacienteService {
 
     @Autowired
     PacienteRepository pacienteRepository;
-    Sort sort = Sort.by("nome").ascending();
-    Pageable pageable = PageRequest.of(0,10,sort);
-    public ResponseEntity <List<PacienteResponseDTO>> listarTodosPacientes(){
-        return ResponseEntity.ok(pacienteRepository.findAll()
-                .stream()
-                .map(PacienteResponseDTO::new)
-                .collect(Collectors.toList())
-        );
-    }
 
-    public ResponseEntity<List<PacienteResponseDTO>> listarPorNomePaciente(String nome) {
-        return ResponseEntity.ok(pacienteRepository.findByNome(nome)
-                .stream()
-                .map(paciente -> new PacienteResponseDTO(paciente.nome(), paciente.CPF(), paciente.email()))
-                .collect(Collectors.toList()));
+    public Page<PacienteResponseDTO> listarPacientesOrdenadosEPageados(Pageable pageable) {
+        Page<Paciente> pacientes = pacienteRepository.findAll(pageable);
+        return pacientes.map(PacienteResponseDTO::new);
     }
-
 
 
     @ResponseStatus(HttpStatus.CREATED)
